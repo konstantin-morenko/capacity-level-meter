90a0b3ac9eca80f4c6b8b7321acc9267abbcb5fc
Konstantin Morenko <konstantin-morenko@users.noreply.github.com>
2020-03-21 20:43:20 +0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,21 @@
+
+#include <xc.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+/* All standard definitions in /opt/microchip/xc8/v2.10/pic/include/pic16f628a.h */
+
+void main(void) {
+  /* ==================== INIT ==================== */
+
+#warning Initialization in __FILE__ is not set
+  /* Revome this after configuring */
+
+  /* ==================== PROGRAM ==================== */
+  while(1) {
+
+#warning Main program in __FILE__ is not set
+    /* Revome this after configuring */
+
+  }
+}
