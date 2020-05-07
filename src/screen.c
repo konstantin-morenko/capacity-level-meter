130842fd96722dc71dc1b32525448a7fdb043dc8
Konstantin Morenko <me@konstantin-morenko.ru>
2020-05-07 17:37:28 +0300

    Arguments renamed

--- src/screen.c
+++ src/screen.c
@@ -28,7 +28,7 @@ inline void scr_flush(void) {
 
 
 /* Преобразует полученный текст в отображение и сохраняет в буфер */
-void scr_text(char t1, char t2, char t3, char t4) {
+void scr_text(char ch4, char ch3, char ch2, char ch1) {
   return;
 }
 
--- src/screen.h
+++ src/screen.h
@@ -2,4 +2,4 @@
 /* Внешние функции индикатора (кроме прерываний) */
 
 
-void scr_text(char t1, char t2, char t3, char t4);
+void scr_text(char ch4, char ch3, char ch2, char ch1);
